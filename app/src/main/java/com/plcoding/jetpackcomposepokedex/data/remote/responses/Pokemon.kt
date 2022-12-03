7bcd2b1b94d0e52e5a03aab35b6bbf7b49aead92
Igor_M <igu_mattos@hotmail.com>
2022-12-03 20:29:07 -0300

    Initial commit

---
+++ app/src/main/java/com/plcoding/jetpackcomposepokedex/data/remote/responses/Pokemon.kt
@@ -0,0 +1,15 @@
+package com.plcoding.jetpackcomposepokedex.data.remote.responses
+
+data class Pokemon(
+    val base_experience: Int,
+    val height: Int,
+    val id: Int,
+    val is_default: Boolean,
+    val location_area_encounters: String,
+    val name: String,
+    val order: Int,
+    val past_types: List<Any>,
+    val stats: List<Stat>,
+    val types: List<Type>,
+    val weight: Int
+)
